31535cafa00670aa341dc19ac87e542bfc4c4e03
chanudevelop <chanu.develop@gmail.com>
2025-12-23 12:01:53 +0900

    Fix: fix start script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-echo "Starting application..."
-cd /home/ec2-user/app/step1
-chmod +x ./*.jar
-# jar 파일 실행 (로그 남기기)
-nohup java -jar ./*SNAPSHOT.jar > /dev/null 2>&1 &
+
+# 1. 배포 폴더로 이동
+REPOSITORY=/home/ec2-user/app/step1
+cd $REPOSITORY
+
+# 2. JAR 파일 이름 찾기
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+# 3. 로그 파일 위치 지정 (CloudWatch가 기다리는 그 파일!)
+LOG_FILE=$REPOSITORY/nohup.out
+
+echo "> JAR Name: $JAR_NAME"
+echo "> Log File: $LOG_FILE"
+
+# 4. 실행 (핵심: /dev/null 대신 $LOG_FILE에 저장해라!)
+nohup java -jar $JAR_NAME > $LOG_FILE 2>&1 &
